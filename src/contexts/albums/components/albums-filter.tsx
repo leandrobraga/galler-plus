d5baf004e5991caf9c789d430e4fa1848f0f0620
Leandro Quadros <leandro.quadros@gmail.com>
2025-11-06 11:27:09 -0300

    Filter by album

--- src/contexts/albums/components/albums-filter.tsx
+++ src/contexts/albums/components/albums-filter.tsx
@@ -1,8 +1,9 @@
 import cx from "classnames";
 import Button from "../../../components/button";
+import Skeleton from "../../../components/skeleton";
 import Text from "../../../components/text";
 import type { Album } from "../models/album";
-import Skeleton from "../../../components/skeleton";
+import usePhotos from "../../photos/hooks/use-photos";
 
 interface AlbumFilterProps extends React.ComponentProps<"div"> {
   albums: Album[];
@@ -13,21 +14,28 @@ export default function AlbumsFilter({
   loading,
   className,
 }: AlbumFilterProps) {
+  const { filters } = usePhotos();
   return (
     <div className={cx("flex items-center gap-3.5 overflow-x-auto", className)}>
       <Text variant="heading-small">Álbuns</Text>
       <div className="flex gap-3">
         {!loading ? (
           <>
-            <Button variant="primary" size="sm" className="cursor-pointer">
+            <Button
+              variant={filters.albumId === null ? "primary" : "ghost"}
+              size="sm"
+              className="cursor-pointer"
+              onClick={() => filters.setAlbumId(null)}
+            >
               Todos
             </Button>
             {albums.map((album) => (
               <Button
                 key={album.id}
-                variant="ghost"
+                variant={filters.albumId === album.id ? "primary" : "ghost"}
                 size="sm"
                 className="cursor-pointer"
+                onClick={() => filters.setAlbumId(album.id)}
               >
                 {album.title}
               </Button>
